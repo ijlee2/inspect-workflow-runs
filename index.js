3623ea22d287bb0fe57e2900a3234d135e24e6ba
ijlee2 <ijlee2@users.noreply.github.com>
2020-08-24 22:27:26 -0500

    Added placeholder code for GitHub client

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 import readEnvironmentVariables from './lib/dotenv';
+import GitHub from './lib/github';
 
 
 async function main() {
   try {
     readEnvironmentVariables();
+
+    const client = new GitHub({
+      repoOwner: 'ijlee2',
+      repoName: 'ember-container-query',
+      workflowFileName: 'ci.yml'
+    });
+
+    console.log(client.repoName);
 
   } catch(error) {
     console.error(error);
